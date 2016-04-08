ed7e796dc0309cabfeb9da946fc3f3c1926701c0
vanessaf <vanessaf@stanford.edu>
2016-04-07 22:47:04 -0700

    Fixed lots of small crashes and bugs, mostly related to ommitted information

--- scrape.py
+++ scrape.py
@@ -1,21 +1,33 @@
 from selenium import webdriver
 from BeautifulSoup import BeautifulSoup
 import re
+from time import sleep
 
 driver = webdriver.PhantomJS()
 
 url = 'https://therapists.psychologytoday.com/rms/prof_detail.php?profid=%s&sid=1459550134.2557_26010&zipcode=94305&search=94305&tr=NextProf'
-prof_id = '207105'
+prof_id = '207105' #Savant
+#prof_id = '95997' #Choy
+#prof_id = '35139' #Truong
+stop_flag = 0
 
-for i in range(1):
+for i in range(10): #controls how many therapist profiles we want to scrape
+	if stop_flag:
+		break
+
 	print '\nID'
 	print '-------------------------'
 	print prof_id
 	driver.get(url % prof_id)
 	soup = BeautifulSoup(driver.page_source)
+	#print soup.prettify()
 
 	#extract the profile linked to by the "next" button (for use the next time through the loop)
-	prof_id = re.search(r'ProfileNav_prevProfLink.*?profid=([0-9]+)', driver.page_source.replace('\n', '')).group(1)
+	try:
+		prof_id = re.search(r'ProfileNav_prevProfLink.*?profid=([0-9]+)', driver.page_source.replace('\n', '')).group(1)
+	except:
+		#case there are no more therapists to process after this one
+		stop_flag = 1
 
 	#extract full name
 	print '\nNAME'
@@ -34,17 +46,30 @@ for i in range(1):
 	#extract phone number
 	print '\nPHONE'
 	print '-------------------------'
-	phone = re.search(r'(?:<a href=\"tel:([0-9]+))', driver.page_source.replace('\n', '')).group(1)
-	print phone
+	match = re.search(r'(?:<a href=\"tel:([0-9]+))', driver.page_source.replace('\n', ''))
+	if match:
+		phone = match.group(1)
+		print phone
 
 	#extract location
 	print '\nLOCATION'
 	print '-------------------------'
-	streetAddress = soup.find('span', {'itemprop': 'streetAddress'}).text
-	zipCode = soup.find('span', {'itemprop': 'postalcode'}).text
-	print streetAddress, zipCode
+	streetAddress = soup.find('span', {'itemprop': 'streetAddress'})
+	zipCode = soup.find('span', {'itemprop': 'postalcode'})
+	if streetAddress and zipCode:
+		print streetAddress.text, zipCode.text
 
-	#extract specialties
+	#extract an additional location, if one exists
+	text = re.search(r'Additional Location.*?<a href="tel:', driver.page_source.replace('\n', ''))
+	if text:
+		text = ' '.join(text.group(0).split())
+		streetAddress2 = re.search(r'> *([0-9]+[A-Za-z ]+)<', text)
+		zipCode2 = re.search(r'"postalcode">([0-9]{5})<', text)
+		if streetAddress2 and zipCode2:
+			print streetAddress2.group(1), zipCode2.group(1)
+
+
+	#extract main specialties
 	print '\nSPECIALTIES'
 	print '-------------------------'
 	for li in soup.findAll('li', {'class':"highlight"}):
@@ -53,67 +78,78 @@ for i in range(1):
 	#extract issues focus
 	print '\nISSUES'
 	print '-------------------------'
-	text = re.search(r'Issues.*?<h3 class="spec-subcat">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for li in text.findAll('li'):
-		print li.text
+	text = re.search(r'Issues</h3>.*?<h', driver.page_source.replace('\n', ''))
+	if text:
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
 
 	#extract mental health focus
 	print '\nMENTAL HEALTH'
 	print '-------------------------'
-	text = re.search(r'Mental Health.*?<h3 class="spec-subcat">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for li in text.findAll('li'):
-		print li.text
+	text = re.search(r'Mental Health.*?<h3 class="spec-subcat">', driver.page_source.replace('\n', ''))
+	if text:
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
 
 	#extract sexuality focus
 	print '\nSEXUALITY'
 	print '-------------------------'
-	text = re.search(r'Sexuality.*?<div class="spec-list">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for li in text.findAll('li'):
-		print li.text
+	text = re.search(r'Sexuality.*?<div class="spec-list">', driver.page_source.replace('\n', ''))
+	if text: 
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
 
-	#extract categories
+	#extract categories 
 	print '\nCATEGORIES'
 	print '-------------------------'
-	text = re.search(r'Categories.*?<div class="spec-list">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for li in text.findAll('li'):
-		print li.text
+	text = re.search(r'Categories.*?<div class="spec-list">', driver.page_source.replace('\n', ''))
+	if text: 
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
+
 	#extract languages other than english
 	print '\nLANGUAGES'
 	print '-------------------------'
-	text = re.search(r'Alternative Languages.*?<h3 class="spec-subcat">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for s in text.findAll('span'):
-		print s.text.replace(',', '')
+	text = re.search(r'Alternative Languages.*?"spec-subcat"', driver.page_source.replace('\n', ''))
+	if text: 
+		text = BeautifulSoup(text.group(0))
+		for s in text.findAll('span'):
+			print s.text.replace(',', '')
 
 	#extract treatment approach
 	print '\nTREATMENT ORIENTATION'
 	print '-------------------------'
-	text = re.search(r'Treatment Orientation.*?<h3 class="spec-subcat">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for b in text.findAll('button', {'class':"ui-button glossary-button"}):
-		print b.text
+	text = re.search(r'Treatment Orientation.*?<h3 class="spec-subcat">', driver.page_source.replace('\n', ''))
+	if text: 
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
 
 	#extract modality
 	print '\nMODALITY'
 	print '-------------------------'
-	text = re.search(r'Modality.*?</div>', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for li in text.findAll('li'):
-		print li.text
+	text = re.search(r'Modality.*?</div>', driver.page_source.replace('\n', ''))
+	if text: 
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
 
 	#extract insurance providers
 	print '\nINSURANCE'
 	print '-------------------------'
-	text = re.search(r'Accepted Insurance Plans.*?<div class="profile-verify-ins">', driver.page_source.replace('\n', '')).group(0)
-	text = BeautifulSoup(text)
-	for li in text.findAll('li'):
-		print li.text
+	text = re.search(r'Accepted Insurance Plans.*?<h', driver.page_source.replace('\n', ''))
+	if text:
+		text = BeautifulSoup(text.group(0))
+		for li in text.findAll('li'):
+			print li.text
 
 
 	print "\n*********************************************"
+
+	sleep(3)
 
 driver.quit()
