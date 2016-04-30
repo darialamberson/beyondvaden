22d60d5fdb440e9e421b5440445f7113463f0464
vanessaf <vanessaf@stanford.edu>
2016-04-29 22:36:01 -0700

    Write tf_idf matrix to file for reuse, added unit testing

--- nlp/CategoryClassifier.py
+++ nlp/CategoryClassifier.py
@@ -3,37 +3,57 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from scipy.sparse import csr_matrix
 import numpy as np
 import math
+import os
 from PorterStemmer import PorterStemmer
+import cPickle as pickle
 
 
 class CategoryClassifier(object):
-	def __init__(self, word_counts_file):
-		with open(word_counts_file, 'r') as f:
-			f.readline()
-			num_cols = len(f.readline().split(','))
-			f.seek(0)
-			word_count_matrix = np.transpose(genfromtxt(f, delimiter=',', skip_header=1, usecols=range(1, num_cols)))
-		self.tfidf_matrix = TfidfTransformer().fit_transform(word_count_matrix).todense()
-		self.idf = np.array([math.log(1 + x) for x in float(len(word_count_matrix))/np.sum(word_count_matrix > 0, axis=0)])
 
-		with open(word_counts_file, 'r') as f:
+
+	def __init__(self, tf_file, tfidf_file, idf_file):
+		if not os.path.isfile(tfidf_file):
+			print '**** Creating tfidf files \n'
+			self.createTfidfFiles(tf_file, tfidf_file, idf_file)
+
+		with open(tf_file, 'r') as f:
 			self.categories = f.readline().strip().split(',')
-			print self.categories
 			self.word_to_index = {}
 			i = 0
 			for line in f: 
 			 	word = line.split(',')[0]
 			 	self.word_to_index[word] = i
 			 	i += 1
-			#print self.word_to_index
 
+		with open(tfidf_file, 'rb') as infile:
+		 	self.tfidf_matrix = pickle.load(infile).todense()
+		
+		self.idf = genfromtxt(idf_file, delimiter = ',')
+
+	def createTfidfFiles(self, tf_file, tfidf_file, idf_file):
+		with open(tf_file, 'r') as f:
+			f.readline()
+			num_cols = len(f.readline().split(','))
+			f.seek(0)
+			word_count_matrix = np.transpose(genfromtxt(f, delimiter = ',', skip_header = 1, usecols = range(1, num_cols)))
+		tfidf_matrix = TfidfTransformer().fit_transform(word_count_matrix)
+		idf = np.array([math.log(1 + x) for x in float(len(word_count_matrix))/np.sum(word_count_matrix > 0, axis = 0)])
+
+		with open(idf_file, 'wb') as f:
+			for i in range(len(idf) - 1):
+				f.write(str(idf[i]))
+				f.write(',')
+			f.write(str(idf[len(idf) - 1]))
+			f.close()
+
+		with open(tfidf_file, 'wb') as f:
+			pickle.dump(tfidf_matrix, f, pickle.HIGHEST_PROTOCOL)
 
 	def classify(self, query):
 		query = "".join(c for c in query if c not in ('!','.',':',',',';','?')).lower()
 		query_words = query.split() 
 		p = PorterStemmer()
 		query_words = [p.stem(query_words[i]) for i in range(len(query_words))]
-		print query_words
 		q = np.zeros(len(self.word_to_index))
 		for word in query_words:
 			if word in self.word_to_index:
--- nlp/testnlp.py
+++ nlp/testnlp.py
@@ -3,12 +3,65 @@ from PorterStemmer import PorterStemmer
 
 
 def main():
-	word_counts_file = 'tf_matrix.csv'
-	query = 'I am struggling in school. I understand nothing. The teachers are mean and the work is boring and hard. I just do not care.'
-	c = CategoryClassifier(word_counts_file=word_counts_file)
-	top_categories = c.classify(query)
-	print top_categories[0:5]
+	tf_file = 'tf_matrix.csv'
+	tfidf_file = 'tf_idf.dat'
+	idf_file = 'idf.csv'
+
+	c = CategoryClassifier(tf_file = tf_file, tfidf_file = tfidf_file, idf_file = idf_file)
+	
+	print 'Test 1: Food keywords\n***********************************'
+	query = 'eating fat weight binge purge whale pounds calories thin skinny cleanse diet puke vomit starve scale toilet anorexic bulimia'
+	expected_categories = ['eating disorders', 'obesity', 'weight loss']
+	testQuery(query, expected_categories, c)
+
+	print 'Test 2: Suicide keywords\n***********************************'
+	query = 'kill die suicide angry depressed knife cut pills gun dead gone sorry note'
+	expected_categories = ['suicidal ideation', 'teen violence', 'depression']
+	testQuery(query, expected_categories, c)
+
+	print 'Test 3: Academic keywords\n***********************************'
+	query = 'school struggle fail classes teachers mean hard boring work grades bad bully read math'
+	expected_categories = ['academic underachievement', 'learning disabilities']
+	testQuery(query, expected_categories, c)
+
+	print 'Test 4: Gay keywords\n***********************************'
+	query = 'pride gay homosexual lgbt sex men closet queer identity'
+	expected_categories = ['gay']
+	testQuery(query, expected_categories, c)
+
+	print 'Test 5: OCD keywords\n***********************************'
+	query = 'obsessive repetitive behavior control germs perfect count ritual'
+	expected_categories = ['obsessive compulsive ocd', 'dog']
+	testQuery(query, expected_categories, c)
+
+#For every expected category with rank i in the top 5, add 1/i to points. We weight a high rank more heavily than a low one.
+#Max points is the score if all n expected categories come first in the top five, ie 1/1 + 1/2 + ... 1/n, where n <= 5. 
+#If we get at least half the max points, pass.
+def testQuery(query, expected_categories, c):
+	print 'expected_categories=', expected_categories
+	max_points = sum(1/float(i+1) for i in range(len(expected_categories)))
+
+	output_categories = c.classify(query)
+	points = 0
+	false_pos = []
+	false_neg = []
+	for i in range(5):
+		if output_categories[i][0] in expected_categories:
+			points += 1/float(i+1)
+			print 'match: ', output_categories[i][0], 'with rank ', i+1
+		else:
+			false_pos.append(output_categories[i][0])
+	for category in expected_categories:
+		if category not in [x[0] for x in output_categories]:
+			false_neg.append(category)
+	print 'false positives =', false_pos
+	print 'false negatives =', false_neg
+	score = points/float(max_points)
+	print 'score =', score
+	print 'PASS\n' if score > .5 else 'FAIL\n'
 
 
 if __name__ == "__main__":
     main()
+
+
