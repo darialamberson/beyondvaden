ece2c9d084174ae67ba340bf138ca76ffb3f11e3
vfriedemann <vanessa.friedemann@gmail.com>
2016-05-12 20:24:47 -0700

    cause getTherapists to return {1,2,3,4,5} for debugging web app while Vanessa develops

--- nlp/FindTherapistsByQuery.py
+++ nlp/FindTherapistsByQuery.py
@@ -1,10 +1,12 @@
 import sys
 from CategoryClassifier import CategoryClassifier
+from RankTherapists import rank
 
 def main(words):
-	c = CategoryClassifier(tf_file='../nlp/tf_matrix.csv', tfidf_file='../nlp/tf_idf.dat', idf_file='../nlp/idf.csv')
+	c = CategoryClassifier(tf_file='../nlp/tf_matrix_working.csv', tfidf_file='../nlp/tf_idf.dat', idf_file='../nlp/idf.csv')
 	categoryAndWeight = c.classify(query=' '.join(words))
-	print ', '.join([x[0] for x in categoryAndWeight[: 3]])
+	rank(categoryAndWeight)
+	#print ', '.join([x[0] for x in categoryAndWeight[: 3]])
 
 if __name__ == '__main__':
 	main(sys.argv[1 :])
--- webapp/FindTherapistsByQuery.rb
+++ webapp/FindTherapistsByQuery.rb
@@ -1,3 +1,11 @@
-query = "eating fat weight binge purge whale pounds calories thin skinny cleanse diet puke vomit starve scale toilet anorexic bulimia"
-f = open("|python ../nlp/FindTherapistsByQuery.py " + query)
-p f.read().strip().split(", ")
+def getTherapists(query)
+	return Array.new() << 1 << 2 << 3 << 4 << 5
+	# f = open("|python ../nlp/FindTherapistsByQuery.py " + query)
+	# return f.read().strip().split(", ")
+end
+
+if __FILE__ == $PROGRAM_NAME
+	query = "eating fat weight binge purge whale pounds calories thin skinny cleanse diet puke vomit starve scale toilet anorexic bulimia"
+	therapists = getTherapists(query)
+	p therapists
+end
