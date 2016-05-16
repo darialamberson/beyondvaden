4b276e91029d37a004dec10dea5b384440d0db43
vfriedemann <vanessa.friedemann@gmail.com>
2016-05-16 10:57:50 -0700

    Fixed formatting of returned value from getTherapists

--- nlp/FindTherapistsByQuery.py
+++ nlp/FindTherapistsByQuery.py
@@ -5,6 +5,8 @@ from RankTherapists import rank
 def main(words):
 	c = CategoryClassifier(tf_file='../nlp/tf_matrix_working.csv', tfidf_file='../nlp/tf_idf.dat', idf_file='../nlp/idf.csv')
 	categoryAndWeight = c.classify(query=' '.join(words))
+	# print categoryAndWeight
+	# print '****************'
 	print rank(categoryAndWeight)
 	#print ', '.join([x[0] for x in categoryAndWeight[: 3]])
 
--- webapp/FindTherapistsByQuery.rb
+++ webapp/FindTherapistsByQuery.rb
@@ -1,11 +1,20 @@
 def getTherapists(query)
 	# return Array.new() << 1 << 2 << 3 << 4 << 5
 	f = open("|python ../nlp/FindTherapistsByQuery.py " + query)
-	return f.read().strip().split(", ")
+	output = f.read().strip().split(", ")
+	therapists = Array.new()
+	for x in output do
+		x.sub!('[', '')
+		x.sub!(']', '')
+		therapists << x.to_i
+	end
+	return therapists
+
 end
 
 if __FILE__ == $PROGRAM_NAME
-	query = "eating fat weight binge purge whale pounds calories thin skinny cleanse diet puke vomit starve scale toilet anorexic bulimia"
+	query = "eating fat weight binge purge pounds calories thin skinny cleanse diet puke vomit starve scale toilet anorexic bulimia"
+	#query = "suicide suicide suicide suicide"
 	therapists = getTherapists(query)
 	p therapists
 end
