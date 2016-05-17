e00b7b3eccfe0be24447e3354becf849a964ddf1
vfriedemann <vanessa.friedemann@gmail.com>
2016-05-17 12:19:05 -0700

    getTherapists changed to getCategoriesAndTherapists in FindTherapistsByQuery.rb, now returning tuple

--- nlp/FindTherapistsByQuery.py
+++ nlp/FindTherapistsByQuery.py
@@ -6,11 +6,10 @@ from RankTherapists import rank
 def main(words):
 	c = CategoryClassifier(tf_file='../nlp/tf_matrix_working.csv', tfidf_file='../nlp/tf_idf.dat', idf_file='../nlp/idf.csv')
 	categoryAndWeight = c.classify(query=' '.join(words))
-	#print ', '.join([x[0] for x in categoryAndWeight[: 3]])
+	print ', '.join([x[0] for x in categoryAndWeight[: 3]])
 
 	scores = rank(categoryAndWeight)
 	print scores
-	# print ','.join(str(x) for x in scores)
 
 if __name__ == '__main__':
 	main(sys.argv[1 :])
--- webapp/FindTherapistsByQuery.rb
+++ webapp/FindTherapistsByQuery.rb
@@ -1,21 +1,30 @@
-def getTherapists(query)
+def getCategoriesAndTherapists(query)
 	# return Array.new() << 1 << 2 << 3 << 4 << 5
 	f = open("|python ../nlp/FindTherapistsByQuery.py " + query)
-	output = f.read().strip().split(", ")
+	output = f.read().strip().gsub("\n", ", ").split(", ")
+	categories = Array.new()
+
+	for x in output[0..2] do
+		x.sub!('[', '')
+		x.sub!(']', '')
+		categories << x
+	end
+
 	therapists = Array.new()
-	for x in output do
+	for x in output[3..-1] do
 		x.sub!('[', '')
 		x.sub!(']', '')
 		therapists << x.to_i
 	end
-	return therapists
+	return [categories,therapists]
 
 end
 
 
 if __FILE__ == $PROGRAM_NAME
 	query = "eating fat weight binge purge pounds calories thin skinny cleanse diet puke vomit starve scale toilet anorexic bulimia"
-	#query = "suicide suicide suicide suicide"
-	therapists = getTherapists(query)
-	p therapists
+	# query = "suicide kill pills cut"
+	output = getCategoriesAndTherapists(query)
+	p output[0]
+	p output[1]
 end
