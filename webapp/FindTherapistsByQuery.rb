021d612eac2b8674f66d5f78903355547be0be12
vfriedemann <vanessa.friedemann@gmail.com>
2016-05-17 13:45:40 -0700

    Commenting

--- webapp/FindTherapistsByQuery.rb
+++ webapp/FindTherapistsByQuery.rb
@@ -1,5 +1,8 @@
+
+#Returns a pair where p[0] is an array containing the 
+#top three categories, and p[1] is an array containing 
+#therapist ids in descending order of relevance
 def getCategoriesAndTherapists(query)
-	# return Array.new() << 1 << 2 << 3 << 4 << 5
 	f = open("|python ../nlp/FindTherapistsByQuery.py " + query)
 	output = f.read().strip().gsub("\n", ", ").split(", ")
 	categories = Array.new()
